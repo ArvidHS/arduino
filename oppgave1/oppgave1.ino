7df55e8def56cade72737f881a8c0605414e38b9
Arvid Håkon Seternes <122388657+ArvidHS@users.noreply.github.com>
2023-02-16 10:34:12 +0100

    upload 2

---
+++ oppgave1/oppgave1.ino
@@ -0,0 +1,19 @@
+int run;
+
+// ******** Setup Here ********
+
+void setup() {
+
+pinMode(13, OUTPUT);
+
+}
+
+
+
+void loop() {
+
+digitalWrite(13, HIGH);
+delay(1000);
+digitalWrite(13, LOW);
+delay(1000);
+}
