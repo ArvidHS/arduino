a0472220cb59ec64a24d933fc17eeccfee4df971
Arvid Håkon Seternes <122388657+ArvidHS@users.noreply.github.com>
2023-02-16 09:37:58 +0100

    upload 1

---
+++ test1/test1.ino
@@ -0,0 +1,105 @@
+
+int run;
+
+// ******** Setup Here ********
+
+void setup() {
+
+
+
+
+  
+
+
+//void setup() {
+
+
+      pinMode(11, OUTPUT);
+      pinMode(12, OUTPUT);
+      pinMode(13, OUTPUT);
+      pinMode(10, OUTPUT);
+      pinMode(9, OUTPUT);
+      pinMode(8, OUTPUT);
+      pinMode(7, OUTPUT);
+      pinMode(3, OUTPUT); 
+      pinMode(2, OUTPUT);
+
+}
+
+
+void loop() {
+
+  
+ digitalWrite(13, HIGH);
+  delay(50);                     
+  digitalWrite(13, LOW);   
+  delay(50);                      
+  digitalWrite(12, HIGH);
+  delay(50);
+  digitalWrite(12, LOW);
+  delay(50);
+  digitalWrite(11, HIGH);
+  delay(50);
+  digitalWrite(11, LOW);
+  delay(50);
+  digitalWrite(10, HIGH);
+  delay(50);
+  digitalWrite(10, LOW);
+  delay(50);
+  digitalWrite(9, HIGH);   
+  delay(50);                       
+  digitalWrite(9, LOW);    
+  delay(50);                      
+  digitalWrite(8, HIGH);
+  delay(50);
+  digitalWrite(8, LOW);
+  delay(50);
+  digitalWrite(7, HIGH);
+  delay(50);
+  digitalWrite(7, LOW);
+  delay(50);
+  digitalWrite(3, HIGH);
+  delay(50);
+  digitalWrite(3, LOW);
+  delay(50);
+  digitalWrite(3, HIGH);  
+  delay(50);                     
+  digitalWrite(3, LOW);    
+  delay(50); 
+  digitalWrite(2, HIGH);
+  delay(50);
+  digitalWrite(2, LOW);
+  delay(50); 
+   digitalWrite(3, HIGH);
+  delay(50);
+  digitalWrite(3, LOW);
+  delay(50);                 
+  digitalWrite(7, HIGH);
+  delay(50);
+  digitalWrite(7, LOW);
+  delay(50);
+  digitalWrite(8, HIGH);
+  delay(50);
+  digitalWrite(8, LOW);
+  delay(50);
+  digitalWrite(9, HIGH);
+  delay(50);
+  digitalWrite(9, LOW);
+  delay(50);
+  digitalWrite(10, HIGH);   
+  delay(50);                       
+  digitalWrite(10, LOW);    
+  delay(50);                      
+  digitalWrite(11, HIGH);
+  delay(50);
+  digitalWrite(11, LOW);
+  delay(50);
+  digitalWrite(12, HIGH);
+  delay(50);
+  digitalWrite(12, LOW);
+  delay(50);
+  digitalWrite(13, HIGH);
+  delay(50);
+  digitalWrite(13, LOW);
+  delay(50);
+}
